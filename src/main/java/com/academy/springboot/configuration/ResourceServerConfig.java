3825a7abbb6b2a68fd7f05aad9936b9d8b040537
John Raven Glomar <jrglomar016@gmail.com>
2022-11-17 14:58:07 +0800

    added authorization

--- src/main/java/com/academy/springboot/configuration/ResourceServerConfig.java
+++ src/main/java/com/academy/springboot/configuration/ResourceServerConfig.java
@@ -23,10 +23,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.
                 anonymous().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.DELETE).access("hasRole('ADMIN')")
-                .antMatchers(HttpMethod.POST).access("hasRole('ADMIN')")
-                .antMatchers(HttpMethod.PUT).access("hasRole('ADMIN')")
-                .antMatchers("/vouchers/**").access("hasRole('ADMIN',USER)")
+                .antMatchers(HttpMethod.POST, "/vouchers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/vouchers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/vouchers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/vouchers/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/vouchers/**").hasAnyRole("ADMIN", "USER")
                 .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO voucher(number, amount, description, type) VALUES (456, 44, 'This is
 INSERT INTO voucher(number, amount, description, type) VALUES (789, 64, 'This is voucher 3', 'CHECK');
 
 INSERT INTO USERS (id, user_name, password,role) VALUES (1, 'admin', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu','ADMIN');
-INSERT INTO USERS (id, user_name, password,role) VALUES (2, 'user', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK','USER');
+INSERT INTO USERS (id, user_name, password,role) VALUES (2, 'user', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu','USER');
