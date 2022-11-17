a0270b4f473114c8c46c10814d5c6ae80bead618
Alexander Gutierrez <AlexanderJamesGutierrez29@gmail.com>
2022-11-18 00:00:42 +0800

    addedSwaggerConfig

--- src/main/java/com/academy/springboot/Week5day1projectApplication.java
+++ src/main/java/com/academy/springboot/Week5day1projectApplication.java
@@ -2,10 +2,12 @@ package com.academy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebMvc
 public class Week5day1projectApplication {
 
 	public static void main(String[] args) {
