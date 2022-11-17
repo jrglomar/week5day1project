00afed153bc4a3ea90678907d987482100ddf876
John Raven Glomar <jrglomar016@gmail.com>
2022-11-17 15:43:22 +0800

    added crud and modified controller

--- src/main/java/com/academy/springboot/service/UserService.java
+++ src/main/java/com/academy/springboot/service/UserService.java
@@ -3,10 +3,19 @@ package com.academy.springboot.service;
 
 import com.academy.springboot.exception.RecordNotFoundException;
 import com.academy.springboot.model.User;
+import com.academy.springboot.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserService {
-    Page<User> findAllUsers(Pageable pageable);
     User findUserById(Long id) throws RecordNotFoundException;
+    
+    Page<User> findAllUser(Pageable pageable);
+
+
+    User saveUser(User user);
+
+    User updateUser(User user, Long id) throws RecordNotFoundException;
+
+    void deleteUser(Long id) throws RecordNotFoundException;
 }
